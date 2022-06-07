852264d38e27851175148c312ef134891b78a294
pbringetto <plbriqto@gmail.com>
2022-06-07 15:21:27 -0700

    channel range

--- src/pyangles/main.py
+++ src/pyangles/main.py
@@ -10,49 +10,36 @@ class Pyangles:
 
     def search(self, df, key, window, order):
         data = []
+        lows, highs = self.range_slopes(df, key, window, order)
         for f in self.functions:
-            data.append({'key': f.__name__, 'value': f.__name__.replace('_' , ' ').capitalize() if f(df, key, window, order) else False})
-        return data
+            data.append({'key': f.__name__, 'value': f.__name__.replace('_' , ' ').capitalize() if f(key,lows, highs) else False})
+        return data, lows, highs
 
-    def ascending_triangle(self, df, key, window, order):
-        lows, highs = self.range_slopes(df, key, window, order)
+    def ascending_triangle(self, key,lows, highs):
         return math.isclose(highs[f'{key}_highs_slope'].iloc[-1], 0) and lows[f'{key}_lows_slope'].iloc[-1] > 0
- 
-    def descending_triangle(self, df, key, window, order):
-        lows, highs = self.range_slopes(df, key, window, order)
+
+    def descending_triangle(self, key,lows, highs):
         return (math.isclose(lows[f'{key}_lows_slope'].iloc[-1], 0)) and (highs[f'{key}_highs_slope'].iloc[-1] < 0)
 
-    def symmetrical_triangle(self, df, key, window, order):
-        lows, highs = self.range_slopes(df, key, window, order)
+    def symmetrical_triangle(self, key,lows, highs):
         return lows[f'{key}_lows_slope'].iloc[-1] > 0 and highs[f'{key}_highs_slope'].iloc[-1] < 0
-        
-    def ascending_channel(self, df, key, window, order):
-        lows, highs = self.range_slopes(df, key, window, order)
-        return lows[f'{key}_lows_slope'].iloc[-1] > 0 and highs[f'{key}_highs_slope'].iloc[-1] > 0 and math.isclose(lows[f'{key}_lows_slope'].iloc[-1], highs[f'{key}_highs_slope'].iloc[-1], rel_tol=100)
-        
-    def descending_channel(self, df, key, window, order):
-        lows, highs = self.range_slopes(df, key, window, order)
-        return lows[f'{key}_lows_slope'].iloc[-1] < 0 and highs[f'{key}_highs_slope'].iloc[-1] < 0 and math.isclose(lows[f'{key}_lows_slope'].iloc[-1], highs[f'{key}_highs_slope'].iloc[-1])
 
-    def ascending_wedge(self, df, key, window, order):
-        lows, highs = self.range_slopes(df, key, window, order)
+    def ascending_channel(self, key,lows, highs):
+        return lows[f'{key}_lows_slope'].iloc[-1] > 0 and highs[f'{key}_highs_slope'].iloc[-1] > 0 and math.isclose(lows[f'{key}_lows_slope'].iloc[-1], highs[f'{key}_highs_slope'].iloc[-1], rel_tol=100)
+
+    def descending_channel(self, key,lows, highs):
+        return lows[f'{key}_lows_slope'].iloc[-1] < 0 and highs[f'{key}_highs_slope'].iloc[-1] < 0 and math.isclose(lows[f'{key}_lows_slope'].iloc[-1], highs[f'{key}_highs_slope'].iloc[-1], rel_tol=100)
+
+    def ascending_wedge(self, key,lows, highs):
         return lows[f'{key}_lows_slope'].iloc[-1] > 0 and highs[f'{key}_highs_slope'].iloc[-1] > 0 and highs[f'{key}_highs_slope'].iloc[-1] > lows[f'{key}_lows_slope'].iloc[-1]
 
-    def descending_wedge(self, df, key, window, order):
-        lows, highs = self.range_slopes(df, key, window, order)
-
-        #print(lows)
-        #print(highs)
-
-
+    def descending_wedge(self, key,lows, highs):
         return lows[f'{key}_lows_slope'].iloc[-1] < 0 and highs[f'{key}_highs_slope'].iloc[-1] < 0 and highs[f'{key}_highs_slope'].iloc[-1] > lows[f'{key}_lows_slope'].iloc[-1]
 
     def range_slopes(self, df, key, window, order):
         low_idx, high_idx = self.high_low_idx(df, key, order)
         lows, highs = df.iloc[low_idx].copy(), df.iloc[high_idx].copy()
-        #lows[f'{key}_lows_slope'] = lows[key].rolling(window=window[0]).apply(self.get_slope, raw=True)
         lows[f'{key}_lows_slope'] = lows[key].rolling(window=window[0]).apply(self.get_slope, raw=True)
-        #highs[f'{key}_highs_slope'] = highs[key].rolling(window=window[1]).apply(self.get_slope, raw=True)
         highs[f'{key}_highs_slope'] = highs[key].rolling(window=window[1]).apply(self.get_slope, raw=True)
         return lows, highs
 
